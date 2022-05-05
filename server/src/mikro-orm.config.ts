1bda4107c9576d8b61ee90a7aedfefe676711388
Hrushikesh Jadhav <rushi611@gmail.com>
2022-05-06 00:02:31 +0530

    MikroORM Setup

---
+++ server/src/mikro-orm.config.ts
@@ -0,0 +1,18 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+
+export default {
+    migrations:{
+        path: path.join(__dirname, "./migrations"),
+        pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
+    entities: [Post],
+    dbName: 'lilReddit',
+    user: 'postgres',
+    password: '0611',
+    type: 'postgresql',
+    debug: !__prod__,
+    allowGlobalContext: true
+} as Parameters<typeof MikroORM.init>[0];
