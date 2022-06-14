14fbf667cac5875f7a0c04a45fca1ba3dec5be75
Hrushikesh Jadhav <rushi611@gmail.com>
2022-06-14 11:49:26 +0530

    URQL Pagination Start

--- client-web/src/pages/index.tsx
+++ client-web/src/pages/index.tsx
@@ -6,7 +6,11 @@ import { usePostsQuery } from "../generated/graphql";
 import { createUqrlClient } from "../utils/createUrqlClient";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery();
+  const [{ data }] = usePostsQuery({
+    variables: {
+      limit: 10,
+    },
+  });
   return (
     <Layout>
       <NextLink href="/create-post">
