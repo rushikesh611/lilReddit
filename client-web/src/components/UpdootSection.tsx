b4f9ebea9175d3e809291974c8f7ac43624def3d
Hrushikesh Jadhav <rushi611@gmail.com>
2022-06-20 20:15:04 +0530

    updoot-ui

---
+++ client-web/src/components/UpdootSection.tsx
@@ -0,0 +1,46 @@
+import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
+import { Flex, IconButton } from "@chakra-ui/react";
+import { useState } from "react";
+import { PostSnippetFragment, useVoteMutation } from "../generated/graphql";
+
+interface UpdootSectionProps {
+  post: PostSnippetFragment;
+}
+
+export const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
+  const [loadingState, setLoadingState] = useState<
+    "updoot-loading" | "downdoot-loading" | "not-loading"
+  >("not-loading");
+  const [, vote] = useVoteMutation();
+  return (
+    <Flex direction="column" justifyContent="center" alignItems="center" mr="4">
+      <IconButton
+        onClick={async () => {
+          setLoadingState("updoot-loading");
+          await vote({
+            postId: post.id,
+            value: 1,
+          });
+          setLoadingState("not-loading");
+        }}
+        isLoading={loadingState === "updoot-loading"}
+        aria-label="updoot post"
+        icon={<ChevronUpIcon h="24px" w="24px" />}
+      />
+      {post.points}
+      <IconButton
+        onClick={async () => {
+          setLoadingState("downdoot-loading");
+          await vote({
+            postId: post.id,
+            value: -1,
+          });
+          setLoadingState("not-loading");
+        }}
+        isLoading={loadingState === "downdoot-loading"}
+        aria-label="downdoot post"
+        icon={<ChevronDownIcon h="24px" w="24px" />}
+      />
+    </Flex>
+  );
+};
--- client-web/src/pages/index.tsx
+++ client-web/src/pages/index.tsx
@@ -1,8 +1,11 @@
+import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
   Flex,
   Heading,
+  Icon,
+  IconButton,
   Link,
   Stack,
   Text,
@@ -11,6 +14,7 @@ import { withUrqlClient } from "next-urql";
 import NextLink from "next/link";
 import { useState } from "react";
 import { Layout } from "../components/Layout";
+import { UpdootSection } from "../components/UpdootSection";
 import { usePostsQuery } from "../generated/graphql";
 import { createUqrlClient } from "../utils/createUrqlClient";
 
@@ -41,11 +45,14 @@ const Index = () => {
       ) : (
         <Stack spacing={8}>
           {data!.posts.posts.map((p) => (
-            <Box key={p.id} p={5} shadow="md" borderWidth="1px">
-              <Heading fontSize="xl">{p.title}</Heading>{" "}
-              <Text>posted by {p.creator.username}</Text>
-              <Text mt={4}>{p.textSnippet}</Text>
-            </Box>
+            <Flex key={p.id} p={5} shadow="md" borderWidth="1px">
+              <UpdootSection post={p} />
+              <Box>
+                <Heading fontSize="xl">{p.title}</Heading>
+                <Text>posted by {p.creator.username}</Text>
+                <Text mt={4}>{p.textSnippet}</Text>
+              </Box>
+            </Flex>
           ))}
         </Stack>
       )}
