7d13dcd210b07a7020bd1fb7c11830f6635cec31
Hrushikesh Jadhav <rushi611@gmail.com>
2022-06-20 20:56:02 +0530

    change-vote

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,3 +1,4 @@
+import { Updoot } from "../entities/Updoot";
 import {
   Arg,
   Ctx,
@@ -50,24 +51,51 @@ export class PostResolver {
     const isUpdoot = value !== -1;
     const realValue = isUpdoot ? 1 : -1;
     const { userId } = req.session;
-    // await Updoot.insert({
-    //   userId,
-    //   postId,
-    //   value: realValue,
-    // });
+    const updoot = await Updoot.findOne({ where: { postId, userId } });
 
-    await getConnection().query(
-      `
-    START TRANSACTION;
+    // the user has voted on the post before
+    // and they are changing their vote
+    if (updoot && updoot.value !== realValue) {
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+          `
+    update updoot
+    set value = $1
+    where "postId" = $2 and "userId" = $3
+        `,
+          [realValue, postId, userId]
+        );
+
+        await tm.query(
+          `
+          update post
+          set points = points + $1
+          where id = $2
+        `,
+          [2 * realValue, postId]
+        );
+      });
+    } else if (!updoot) {
+      // has never voted before
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+          `
     insert into updoot ("userId", "postId", value)
-    values (${userId},${postId},${realValue});
-    update post
-    set points = points + ${realValue}
-    where id = ${postId};
-    COMMIT;
-    `
-    );
+    values ($1, $2, $3)
+        `,
+          [userId, postId, realValue]
+        );
 
+        await tm.query(
+          `
+    update post
+    set points = points + $1
+    where id = $2
+      `,
+          [realValue, postId]
+        );
+      });
+    }
     return true;
   }
 
